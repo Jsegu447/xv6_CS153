614b1f5b5f913eee7d0d4b88980cb1bfdc3f807f
Jonas Segundo <jsegu010@sledge.cs.ucr.edu>
2022-01-30 15:47:19 -0800

    revrt changes to wait in proc.c

--- proc.c
+++ proc.c
@@ -282,7 +282,7 @@ wait(int *status) //LAB1 WAIT
     // Scan through table looking for exited children.
     havekids = 0;
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->pid != pid)
+      if(p->parent != curproc)
         continue;
       havekids = 1;
       if(p->state == ZOMBIE){
